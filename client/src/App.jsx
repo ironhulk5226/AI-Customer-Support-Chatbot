8fce2551e678cee08d0b81f380ee239bf6a2e67f
Aftab-23 <khanarman7593@gmail.com>
2026-09-23 18:25:12 +0530

    Customer/admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,34 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CustomerHome from "./pages/CustomerHome";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminLogin from "./pages/AdminLogin";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
 
 function App() {
     return (
         <Routes>
+            {/* Customer Login */}
             <Route path="/login" element={<Login />} />
 
+            {/* Customer Registration */}
             <Route path="/register" element={<Register />} />
 
+            {/* Admin Login */}
+            <Route path="/admin-login" element={<AdminLogin />} />
+
+            {/* Customer routes */}
             <Route element={<ProtectedRoute />}>
                 <Route path="/customer" element={<CustomerHome />} />
             </Route>
 
+            {/* Admin routes */}
+            <Route element={<AdminRoute />}>
+                <Route path="/admin" element={<AdminDashboard />} />
+            </Route>
+
+            {/* Default route */}
             <Route
                 path="/"
                 element={<Navigate to="/login" replace />}
